76c22f90d22603e738746454ad77146df06e8f49
Kerri-Ann <kaybee1jam@yahoo.com>
2020-05-01 12:39:19 -0400

    added new challenge. Organized files in folders.

--- twoSum/two_sum.js
+++ twoSum/two_sum.js
@@ -4,13 +4,6 @@
  * @return {number[]}
  */
 
-
-// DESCRIPTION
-// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-// Given nums = [2, 7, 11, 15], target = 9, Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1]
-
-
 // Loop thru array of nums.
 // Starting with first index, find next operand by subtracting index from target
 // Check if that operand is in array and it is not itself, if it is return i and the index of the operand
